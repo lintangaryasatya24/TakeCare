37a227825c2a29ace13900b0d2131de86ddef65c
prawirowg <47909526+prawirowg@users.noreply.github.com>
2020-04-23 17:34:03 +0700

    add kontak.php

---
+++ application/views/kontak.php
@@ -0,0 +1,24 @@
+<div class="about-section paddingTB60">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 col-sm-6">
+                <div class="about-title clearfix">
+                    <h1>Find<span>Us</span></h1>
+                    <h3>TakeCare caring with heart.</h3>
+                    <p class="about-paddingB">Telepon : 022-0123456<br>
+                    Jl.Jend. Gatot Subroto No.123 Bandung</p>
+                    <div class="about-icons">
+                        <ul>
+                            <li><a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a> </li>
+                            <li><a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a> </li>
+                            <li> <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a> </li>
+                            <li> <a href="mailto:admin@takecare.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+ 
