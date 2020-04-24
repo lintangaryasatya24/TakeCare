627dfafa42d9390a0829f13a98b4e9586e003fd4
dhuhitatm <64154007+dhuhitatm@users.noreply.github.com>
2020-04-24 15:43:32 +0700

    Add files via upload

---
+++ application/views/berhasil.php
@@ -0,0 +1,20 @@
+<div class="container">
+<h2>Order sukses</h2>
+<hr>
+<p>Terima kasih sudah berbelanja di TakeCare Shop. Orderan sudah diterima, Mohon menunggu barang sampai kerumah anda dengan aman.<br>
+Jika melebihi batas waktu silahkan kontak admin.</p>
+</div>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ 
