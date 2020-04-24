cdb352dbab54e866a09012f8fc4d63e620c4d2f3
dhuhitatm <64154007+dhuhitatm@users.noreply.github.com>
2020-04-24 15:48:32 +0700

    Add files via upload
    add aboutus.php

---
+++ application/views/aboutus.php
@@ -0,0 +1,22 @@
+<div class="about-section paddingTB60">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 col-sm-6">
+                <div class="about-title clearfix">
+                    <h1>About <span>TakeCare</span></h1>
+                    <h3>TakeCare caring with heart.</h3>
+                    <p class="about-paddingB">Kesehatan adalah hal yang harus kita jaga dengan baik. Apapun yang terjadi ksehatan adalah yang paling penting karena kalau kita tidak sehat,kita tidak dapat melakukan apapun dengan baik. Dan kalau kita tidak menjaga kesehatan kita mulai sekarang,kita pasti akan menyesal di kemudian hari.</p>
+                    <p>Maka dari sanalah berdirinya TakeCare, TakeCare adalah pelayanan berbasis web yang menyediakan berbagai layanan seperti beli obat,chat dengan dokter dan lain-lain.</p>
+                    <div class="about-icons">
+                        <ul>
+                            <li><a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a> </li>
+                            <li><a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a> </li>
+                            <li> <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a> </li>
+                            <li> <a href="mailto:admin@takecare.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
